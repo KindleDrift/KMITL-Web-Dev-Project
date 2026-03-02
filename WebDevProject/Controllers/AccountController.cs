26f01ceb89c58323b97512c05b8fb29bc1a73a01
Phudith Janjaeng <136161191+KindleDrift@users.noreply.github.com>
2026-03-02 21:44:52 +0700

    Add asp identity login feature

--- WebDevProject/Controllers/AccountController.cs
+++ WebDevProject/Controllers/AccountController.cs
@@ -1,15 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebDevProject.Models;
 
 namespace WebDevProject.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Signin()
+        private readonly SignInManager<Users> _signInManager;
+        private readonly UserManager<Users> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
         {
             return View();
         }
 
-        public IActionResult Signup()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignIn(SignInViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
         {
             return View();
         }
---
+++ WebDevProject/Models/Users.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebDevProject.Models
+{
+    public class Users : IdentityUser
+    {
+        public string? DisplayName { get; set; }
+    }
+}
