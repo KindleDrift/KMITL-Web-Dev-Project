7b5cf1634cd05fed622b684c806e16dd8843ef5c
Phudith Janjaeng <136161191+KindleDrift@users.noreply.github.com>
2026-03-04 16:15:35 +0700

    Fixed file to use cshtml and updated Profile Controller

---
+++ WebDevProject/Controllers/ProfileController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebDevProject.Models;
+
+namespace WebDevProject.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<Users> _userManager;
+
+        public ProfileController(UserManager<Users> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return RedirectToAction("SignIn", "Account");
+            }
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return RedirectToAction("SignIn", "Account");
+            }
+
+            return View(user);
+        }
+    }
+}
