e0c659bf1cf8c699e0abbf1963928c6e7dfc379e
Phudith Janjaeng <136161191+KindleDrift@users.noreply.github.com>
2026-03-04 14:33:49 +0700

    Fixed to use cshtml, and linked with View Controller for now. And renamed to Board/Search

---
+++ WebDevProject/Controllers/BoardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDevProject.Controllers
+{
+    public class BoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search() { 
+            return View();
+        }
+    }
+}
