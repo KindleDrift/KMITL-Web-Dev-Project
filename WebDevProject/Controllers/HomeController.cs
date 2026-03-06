ce97e96a193b8d7a51229b90c8be685e74cb7353
Phudith Janjaeng <136161191+KindleDrift@users.noreply.github.com>
2026-03-06 18:09:45 +0700

    Added Admin Page for editing users

--- WebDevProject/Controllers/HomeController.cs
+++ WebDevProject/Controllers/HomeController.cs
@@ -22,8 +22,20 @@ namespace WebDevProject.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int? id)
         {
+            var statusCode = id ?? 500;
+            var originalPath = HttpContext.Request.Path.Value ?? "";
+
+            // Return 404 view for unauthorized/unauthenticated access to admin routes
+            if ((statusCode == 401 || statusCode == 403) && originalPath.StartsWith("/Admin"))
+            {
+                Response.StatusCode = 404;
+                return View("Error404", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+
+            // Return normal error page for other cases
+            Response.StatusCode = statusCode;
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
