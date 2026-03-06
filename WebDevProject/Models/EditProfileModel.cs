9a779ed051882f8b8d10e9687f87efd433732467
Flum0671 <67010671@kmitl.ac.th>
2026-03-07 01:22:28 +0700

    add working edit profile

---
+++ WebDevProject/Models/EditProfileModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDevProject.Models
+{
+    public class EditProfileViewModel
+    {
+        // Profile Image upload
+        public IFormFile? ProfileImage { get; set; }
+
+        // Current profile picture URL (for display)
+        public string? CurrentProfilePictureUrl { get; set; }
+
+        public required string UserName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+
+        public Users.Gender? UserGender { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public required string Email { get; set; }
+
+    }
+}
