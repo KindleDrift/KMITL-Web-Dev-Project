71c669c6bc7a7f475ed00b0bc10a8e4eb0eb2f6f
Phudith Janjaeng <136161191+KindleDrift@users.noreply.github.com>
2026-03-07 01:03:36 +0700

    Switched back the Set<Board> in DbContext, since it is a modern practice for Db

--- WebDevProject/Data/ApplicationDbContext.cs
+++ WebDevProject/Data/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace WebDevProject.Data
         {
         }
 
-        public DbSet<Board> Boards { get; set; }
+        public DbSet<Board> Boards => Set<Board>();
 
         public DbSet<BoardParticipant> BoardParticipants => Set<BoardParticipant>();
 
